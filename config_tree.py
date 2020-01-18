b1b191690051e640518c25dd860b7c6e4525fcb7
TAC <tac@tac42.net>
2020-01-18 10:04:18 +0900

    Add load and merge config values

--- config_tree.py
+++ config_tree.py
@@ -3,6 +3,7 @@
 # Config Tree - Structure
 
 from pathlib import Path
+import yaml
 import os
 
 
@@ -22,13 +23,23 @@ def create_config_tree_sub(dir_path: Path, conf_file_name: str, current_config: 
     conf_file_path = dir_path / conf_file_name
     if Path.exists(conf_file_path):
         # Todo: actual load config file
-        config_tree[str(dir_path)] = {"conf": "config!!"}
+        try:
+            parsed = parse_config(conf_file_path)
+            current_config = dict(current_config, **parsed)
+            config_tree[str(dir_path)] = current_config
+        except Exception as e:
+            print(e)
 
     # Recursive load
     for d in [d for d in dir_path.iterdir() if d.is_dir()]:
         create_config_tree_sub(d, conf_file_name, current_config, config_tree)
 
     return config_tree
+
+
+def parse_config(file_path: Path):
+    with file_path.open() as f:
+        return yaml.safe_load(f)
 
 
 config_tree = create_config_tree("sample")
