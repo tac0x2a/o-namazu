db89a5dcbffa48205651abef6e83fa7d045a828c
TAC <tac@tac42.net>
2020-01-18 11:01:16 +0900

    Rename tree to map

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ import yaml
 import os
 
 
-def create_config_tree(root_dir_path: str, conf_file: str = ".onamazu") -> dict:
+def create_config_map(root_dir_path: str, conf_file: str = ".onamazu") -> dict:
     """
     Load config parameters each dirs.
     Config file should be written in YAML format.
 
     Usage:
-        config_tree = create_config_tree("./sample")
-        print(config_tree) # {'sample': {'pattern': '*.json'}, 'sample/hoge': {'pattern': '*.csv', 'piyo': 'piyo'},...
+        config_map = create_config_map("./sample")
+        print(config_map) # {'sample': {'pattern': '*.json'}, 'sample/hoge': {'pattern': '*.csv', 'piyo': 'piyo'},...
 
     Arguments:
         root_dir_path {str} -- Path to root dir.
@@ -23,31 +23,31 @@ def create_config_tree(root_dir_path: str, conf_file: str = ".onamazu") -> dict:
         dict -- Key: dir path, Value: dict of parameters.
     """
 
-    config_tree = {}
+    config_map = {}
 
-    create_config_tree_sub(Path(root_dir_path), conf_file, {}, config_tree)
+    create_config_map_sub(Path(root_dir_path), conf_file, {}, config_map)
 
-    return config_tree
+    return config_map
 
 
-def create_config_tree_sub(dir_path: Path, conf_file_name: str, current_config: dict, config_tree: dict) -> dict:
+def create_config_map_sub(dir_path: Path, conf_file_name: str, current_config: dict, config_map: dict) -> dict:
     if not Path.is_dir(dir_path):
-        return config_tree
+        return config_map
 
     conf_file_path = dir_path / conf_file_name
     if Path.exists(conf_file_path):
         try:
             parsed = parse_config(conf_file_path)
             current_config = dict(current_config, **parsed)  # overwrite parent dir config by current dir config
-            config_tree[str(dir_path)] = current_config
+            config_map[str(dir_path)] = current_config
         except Exception as e:
             print(e)
 
     # Recursive load
     for d in [d for d in dir_path.iterdir() if d.is_dir()]:
-        create_config_tree_sub(d, conf_file_name, current_config, config_tree)
+        create_config_map_sub(d, conf_file_name, current_config, config_map)
 
-    return config_tree
+    return config_map
 
 
 def parse_config(file_path: Path) -> dict:
