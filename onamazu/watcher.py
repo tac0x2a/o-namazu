e1ef544a831de6ce7105f6909c186f8211af3611
TAC <tac@tac42.net>
2020-01-22 22:02:32 +0900

    Fix duplicated modified message

--- onamazu/watcher.py
+++ onamazu/watcher.py
@@ -11,25 +11,34 @@ import fnmatch
 from . import config as cfg
 
 
+class NamazuEvent():
+    def __init__(self, event):
+        self.event = event
+        self.src_path = event.src_path
+        self.created_at = time.time()
+
+
 class NamazuHandler(PatternMatchingEventHandler):
     def __init__(self, patterns, config, callback):
         patterns = list(set(patterns))
         super(NamazuHandler, self).__init__(patterns=patterns)
         self.config = config
         self.callback = callback
+        self.last_modified = {}
 
     def on_moved(self, event):
         print(f"on_moved:{event.src_path} -> {event.dest_path}")
 
     def on_created(self, event):
         print(f"on_created:{event.src_path}")
+        # self.previouse_created = event
 
     def on_deleted(self, event):
         print(f"on_deleted:{event.src_path}")
 
     def on_modified(self, event):
         print(f"on_modified:{event.src_path}")
-        self.judge(event)
+        self.judge(NamazuEvent(event))
 
     def judge(self, event):
         src_path = Path(event.src_path)
@@ -40,9 +49,16 @@ class NamazuHandler(PatternMatchingEventHandler):
         if 'pattern' not in conf:
             return
 
+        # Ignore duplicated modified
+        if event.src_path in self.last_modified and \
+           time.time() - self.last_modified[event.src_path].created_at < 1:  # replace value by config
+            print(f"Ignore {event.src_path}")
+            return  # ignore
+
         pattern = conf['pattern']
 
         if fnmatch.fnmatch(file_name, pattern):
+            self.last_modified[event.src_path] = event
             self.callback(event)
 
 
