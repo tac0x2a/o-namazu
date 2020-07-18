ca4bdd907540b4c7878f582959d5a0c61cdf8b77
TAC <tac@tac42.net>
2020-07-18 14:23:19 +0900

    Implements store last read pos

--- onamazu/csv_handler.py
+++ onamazu/csv_handler.py
@@ -1,15 +1,54 @@
+import yaml
+from pathlib import Path
+from threading import Lock
 
 
-def read_all(file_paht: str, conf) -> str:
-    (body, pos) = read_tail(file_paht, 0, conf)
+DB_FILE = '.onamazu.db'
+Lock = Lock()
+
+
+def read(file_path: Path, conf) -> str:
+
+    if Path.is_dir(file_path):
+        raise ValueError(f"Unexpected path '{str(file_path)}'. Directory path is not allowed.")
+
+    # Update last read pos
+
+    db_file_path = file_path.parent / DB_FILE
+    with Lock:
+        if not db_file_path.exists():
+            _store_db(db_file_path, {
+                "watching": {}
+            })
+
+        db = _load_db(db_file_path)
+        body = _read_with_db(file_path, db, conf)
+        _store_db(db_file_path, db)
+
+        return body
+
+
+def _read_with_db(file_path: Path, db_dict: dict, conf: dict) -> str:
+
+    watching = db_dict["watching"]
+    file_name = file_path.name
+
+    if file_name not in watching.keys():
+        watching[file_name] = {
+            "read_completed_pos": 0
+        }
+
+    pos = watching[file_name]["read_completed_pos"]
+    (body, new_pos) = _read_tail(file_path, pos, conf)
+    watching[file_name]["read_completed_pos"] = new_pos
+
     return body
 
 
-def read_tail(file_paht: str, already_read_pos: int, conf) -> (str, int):
+def _read_tail(file_path: Path, already_read_pos: int, conf) -> (str, int):
 
-    with open(file_paht) as f:
+    with file_path.open() as f:
         read_string = f.readline()  # header
-        print(f'header:{read_string}')
         current = max(f.tell(), already_read_pos)
         f.seek(current)
 
@@ -17,3 +56,13 @@ def read_tail(file_paht: str, already_read_pos: int, conf) -> (str, int):
 
         return (read_string, f.tell())
 
+
+def _load_db(file_path: Path) -> dict:
+
+    with file_path.open() as f:
+        return yaml.safe_load(f)
+
+
+def _store_db(file_path: Path, db: dict):
+    with file_path.open("w") as f:
+        return yaml.dump(db, f)
--- tests/test_csv_handler.py
+++ tests/test_csv_handler.py
@@ -1,10 +1,8 @@
 
-from onamazu import csv_handler as ch
-
-from onamazu import config
-from . import conftest as ct
-
 import textwrap as tw
+from pathlib import Path
+from onamazu import csv_handler as ch, config
+from . import conftest as ct
 
 
 def test_csv_read_all_return_all_data():
@@ -19,7 +17,7 @@ def test_csv_read_all_return_all_data():
         3,4
         """).lstrip()
 
-    actual = ch.read_all(file_path, conf)
+    (actual, pos) = ch._read_tail(Path(file_path), 0, conf)
 
     assert expected == actual
 
@@ -35,7 +33,7 @@ def test_csv_tail_return_appended_data():
         1,2
         3,4
         """).lstrip()
-    (actual_head, pos_head) = ch.read_tail(file_path, 0, conf)
+    (actual_head, pos_head) = ch._read_tail(Path(file_path), 0, conf)
     assert expected_head == actual_head
 
     # Append new lines
@@ -47,7 +45,79 @@ def test_csv_tail_return_appended_data():
         7,8
         9,10
         """).lstrip()
-    (actual_appended, pos_appended) = ch.read_tail(file_path, pos_head, conf)
-
+    (actual_appended, pos_appended) = ch._read_tail(Path(file_path), pos_head, conf)
     assert expected_appended == actual_appended
 
+
+def test_csv_read_return_appended_data():
+    ct.place_config_file("", {"pattern": "*.csv"})
+    conf = config.create_config_map(ct.ROOT_DIR)[ct.ROOT_DIR]
+
+    file_path = ct.write_csv("", "test.csv", [("hello", "world"), (1, 2), (3, 4)])
+    expected_head = tw.dedent(
+        """
+        hello,world
+        1,2
+        3,4
+        """).lstrip()
+    actual_head = ch.read(Path(file_path), conf)
+    assert expected_head == actual_head
+
+    # Append new lines
+    file_path = ct.write_csv("", "test.csv", [(5, 6), (7, 8), (9, 10)])
+    expected_appended = tw.dedent(
+        """
+        hello,world
+        5,6
+        7,8
+        9,10
+        """).lstrip()
+    actual_appended = ch.read(Path(file_path), conf)
+    assert expected_appended == actual_appended
+
+
+def test_csv_read_return_appended_data_each_file():
+    ct.place_config_file("", {"pattern": "*.csv"})
+    conf = config.create_config_map(ct.ROOT_DIR)[ct.ROOT_DIR]
+
+    file_path_a = ct.write_csv("", "test_a.csv", [("hello", "world"), (1, 2), (3, 4)])
+    expected_head_a = tw.dedent(
+        """
+        hello,world
+        1,2
+        3,4
+        """).lstrip()
+    actual_head_a = ch.read(Path(file_path_a), conf)
+    assert expected_head_a == actual_head_a
+
+    file_path_b = ct.write_csv("", "test_b.csv", [("hoge", "fuga"), (1, 2), (3, 4), (5, 6)])
+    expected_head_b = tw.dedent(
+        """
+        hoge,fuga
+        1,2
+        3,4
+        5,6
+        """).lstrip()
+    actual_head_b = ch.read(Path(file_path_b), conf)
+    assert expected_head_b == actual_head_b
+
+    # Append new lines
+    file_path_a = ct.write_csv("", "test_a.csv", [(5, 6), (7, 8)])
+    expected_appended_a = tw.dedent(
+        """
+        hello,world
+        5,6
+        7,8
+        """).lstrip()
+    actual_appended_a = ch.read(Path(file_path_a), conf)
+    assert expected_appended_a == actual_appended_a
+
+    file_path_b = ct.write_csv("", "test_b.csv", [(7, 8), ("hoge", "fuga")])
+    expected_appended_b = tw.dedent(
+        """
+        hoge,fuga
+        7,8
+        hoge,fuga
+        """).lstrip()
+    actual_appended_b = ch.read(Path(file_path_b), conf)
+    assert expected_appended_b == actual_appended_b
