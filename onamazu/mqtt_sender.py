5eb45b02ecda80ffc928a4570fc3205f3eb6d157
TAC <tac@tac42.net>
2020-07-24 16:50:58 +0900

    Implement to send multiple messages

--- onamazu/mqtt_sender.py
+++ onamazu/mqtt_sender.py
@@ -1,28 +1,10 @@
-import paho.mqtt.client as mqtt
+import paho.mqtt.publish as pub
 import logging
 
 logger = logging.getLogger("mqtt_sender")
 
 
-class MQTT_Sender:
-    def __init__(self):
-        pass
-
-    def connect(self, host: str, port):
-        self.client = mqtt.Client()                      # クラスのインスタンス(実体)の作成
-        self.client.on_connect = self.on_connect         # 接続時のコールバック関数を登録
-        self.client.on_disconnect = self.on_disconnect  # 切断時のコールバックを登録
-        self.client.on_publish = self.on_publish         # メッセージ送信時のコールバック
-        self.client.connect(host, port, 60)
-
-    def send(self, json_data: str, topic: str):
-        self.client.publish(topic, json_data)
-
-    def on_connect(self, client, userdata, flag, rc):
-        logger.info("connected {}".format(str(rc)))
-
-    def on_disconnect(self, client, userdata, flag, rc):
-        logger.info("disconnected")
-
-    def on_publish(self, client, userdata, mid):
-        logger.debug(f"Sent_MID: {mid}")
+def publish(payloads: list, client_id: str, topic: str, hostname: str, port: int, qos: int = 0, retain: bool = False):
+    # msg = {'topic':"<topic>", 'payload':"<payload>", 'qos':<qos>, 'retain':<retain>}
+    messages = [{'topic': topic, 'payload': p, 'qos': qos, 'retain': retain} for p in payloads]
+    pub.multiple(messages, hostname, port, client_id)
