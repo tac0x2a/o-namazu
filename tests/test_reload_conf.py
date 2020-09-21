ee556781b5d0aeda9573e7c5a598a2d70693907b
TAC <tac@tac42.net>
2020-09-21 17:12:41 +0900

    Rename callback method name

--- tests/test_reload_conf.py
+++ tests/test_reload_conf.py
@@ -9,7 +9,7 @@ def test_reload_on_create():
     ct.place_config_file("", {"pattern": "*.csv"})
 
     o = ONamazu(ct.ROOT_DIR, 60)
-    o.sample_handler = MagicMock(name="sample_handler")
+    o.event_handler = MagicMock(name="event_handler")
     o.click()
 
     ct.place_file("sub", "sample01.csv", "hello,world1")
@@ -18,13 +18,13 @@ def test_reload_on_create():
     ct.place_config_file("sub", {"pattern": "*.csv"})
     o.click(); o.click(); o.click()  # apply
 
-    o.sample_handler.assert_not_called()
+    o.event_handler.assert_not_called()
     ct.place_file("sub", "sample02.csv", "hello,world2")
     o.click()
     o.stop()
 
-    o.sample_handler.assert_called_once_with(ANY)
-    ev, = o.sample_handler.call_args.args
+    o.event_handler.assert_called_once_with(ANY)
+    ev, = o.event_handler.call_args.args
     assert ev.src_path == "/".join([ct.ROOT_DIR, "sub", "sample02.csv"])
 
 
@@ -32,7 +32,7 @@ def test_reload_on_modified():
     ct.place_config_file("", {"pattern": "*.jsonl"})
 
     o = ONamazu(ct.ROOT_DIR, 60)
-    o.sample_handler = MagicMock(name="sample_handler")
+    o.event_handler = MagicMock(name="event_handler")
     o.click()
 
     ct.place_file("", "sample01.csv", "hello,world1")
@@ -41,13 +41,13 @@ def test_reload_on_modified():
     ct.place_config_file("", {"pattern": "*.csv"})
     o.click(); o.click(); o.click()  # apply
 
-    o.sample_handler.assert_not_called()
+    o.event_handler.assert_not_called()
     ct.place_file("", "sample02.csv", "hello,world2")
     o.click()
     o.stop()
 
-    o.sample_handler.assert_called_once_with(ANY)
-    ev, = o.sample_handler.call_args.args
+    o.event_handler.assert_called_once_with(ANY)
+    ev, = o.event_handler.call_args.args
     assert ev.src_path == "/".join([ct.ROOT_DIR, "sample02.csv"])
 
 
@@ -56,7 +56,7 @@ def test_reload_on_delete():
     ct.place_config_file("sub", {"pattern": "*.csv"})
 
     o = ONamazu(ct.ROOT_DIR, 60)
-    o.sample_handler = MagicMock(name="sample_handler")
+    o.event_handler = MagicMock(name="event_handler")
     o.click()
 
     ct.place_file("sub", "sample01.csv", "hello,world1")
@@ -69,6 +69,6 @@ def test_reload_on_delete():
     o.click()
     o.stop()
 
-    o.sample_handler.assert_called_once_with(ANY)
-    ev, = o.sample_handler.call_args.args
+    o.event_handler.assert_called_once_with(ANY)
+    ev, = o.event_handler.call_args.args
     assert ev.src_path == "/".join([ct.ROOT_DIR, "sub", "sample01.csv"])
