173596a6c5c8b4205e4b89209973d48e5d518386
TAC <tac@tac42.net>
2020-09-21 17:04:44 +0900

    Improve reload test

--- tests/test_reload_conf.py
+++ tests/test_reload_conf.py
@@ -2,24 +2,73 @@
 from unittest.mock import MagicMock, ANY
 
 from . import conftest as ct
-
 from onamazu.onamazu import ONamazu
 
 
-def test_reload_on_modify():
+def test_reload_on_create():
+    ct.place_config_file("", {"pattern": "*.csv"})
+
+    o = ONamazu(ct.ROOT_DIR, 60)
+    o.sample_handler = MagicMock(name="sample_handler")
+    o.click()
+
+    ct.place_file("sub", "sample01.csv", "hello,world1")
+    o.click()
+
+    ct.place_config_file("sub", {"pattern": "*.csv"})
+    o.click(); o.click(); o.click()  # apply
+
+    o.sample_handler.assert_not_called()
+    ct.place_file("sub", "sample02.csv", "hello,world2")
+    o.click()
+    o.stop()
+
+    o.sample_handler.assert_called_once_with(ANY)
+    ev, = o.sample_handler.call_args.args
+    assert ev.src_path == "/".join([ct.ROOT_DIR, "sub", "sample02.csv"])
+
+
+def test_reload_on_modified():
     ct.place_config_file("", {"pattern": "*.jsonl"})
 
     o = ONamazu(ct.ROOT_DIR, 60)
     o.sample_handler = MagicMock(name="sample_handler")
     o.click()
+
     ct.place_file("", "sample01.csv", "hello,world1")
     o.click()
+
     ct.place_config_file("", {"pattern": "*.csv"})
-    o.click()
-    o.click()
-    o.click()  # apply
+    o.click(); o.click(); o.click()  # apply
+
+    o.sample_handler.assert_not_called()
     ct.place_file("", "sample02.csv", "hello,world2")
     o.click()
     o.stop()
 
     o.sample_handler.assert_called_once_with(ANY)
+    ev, = o.sample_handler.call_args.args
+    assert ev.src_path == "/".join([ct.ROOT_DIR, "sample02.csv"])
+
+
+def test_reload_on_delete():
+    ct.place_config_file("", {"pattern": "*.csv"})
+    ct.place_config_file("sub", {"pattern": "*.csv"})
+
+    o = ONamazu(ct.ROOT_DIR, 60)
+    o.sample_handler = MagicMock(name="sample_handler")
+    o.click()
+
+    ct.place_file("sub", "sample01.csv", "hello,world1")
+    o.click()
+
+    ct.delete_config_file("sub")
+    o.click(); o.click(); o.click()  # apply
+
+    ct.place_file("sub", "sample02.csv", "hello,world2")
+    o.click()
+    o.stop()
+
+    o.sample_handler.assert_called_once_with(ANY)
+    ev, = o.sample_handler.call_args.args
+    assert ev.src_path == "/".join([ct.ROOT_DIR, "sub", "sample01.csv"])
