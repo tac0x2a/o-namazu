7a3e18789fa0db94231a192b88acd036e1d59173
TAC <tac@tac42.net>
2020-09-21 16:41:28 +0900

    Add reload test

--- tests/test_reload_conf.py
+++ tests/test_reload_conf.py
@@ -1,26 +1,25 @@
 
+from unittest.mock import MagicMock, ANY
+
 from . import conftest as ct
-from onamazu import config
-from onamazu import watcher
+
+from onamazu.onamazu import ONamazu
 
 
 def test_reload_on_modify():
     ct.place_config_file("", {"pattern": "*.jsonl"})
-    conf = config.create_config_map(ct.ROOT_DIR)
-    events = []
 
-    w = watcher.NamazuWatcher(ct.ROOT_DIR, conf, lambda ev: events.append(ev))
-    w.start()
-
-    ct.place_file("", "sample.csv", "hello,world1")
-    w.wait(1)
+    o = ONamazu(ct.ROOT_DIR, 60)
+    o.sample_handler = MagicMock(name="sample_handler")
+    o.click()
+    ct.place_file("", "sample01.csv", "hello,world1")
+    o.click()
     ct.place_config_file("", {"pattern": "*.csv"})
-    w.wait(1)
-    ct.place_file("", "sample.csv", "hello,world2")
-    w.wait(1)
+    o.click()
+    o.click()
+    o.click()  # apply
+    ct.place_file("", "sample02.csv", "hello,world2")
+    o.click()
+    o.stop()
 
-    w.stop()
-
-    expected = 1
-    actual = len(events)
-    assert expected == actual
+    o.sample_handler.assert_called_once_with(ANY)
